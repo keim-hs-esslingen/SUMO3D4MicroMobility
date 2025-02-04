a00d7a0bd101c1d5e46cec1f8757aedbe2c49a9b
Emanuel <62055778+DrRembo@users.noreply.github.com>
2025-02-04 16:18:18 +0100

    Added SOC and 3DPosition to  runMicroMobilityScenario.py

--- StuttgartSouthModel/runMicroMobilityScenario.py
+++ StuttgartSouthModel/runMicroMobilityScenario.py
@@ -6,6 +6,16 @@ import traci
 
 traci.start(["sumo-gui", "-c", "osm.sumocfg"])
 
+# read maximum battery capacity
+maxBatteryCapacity= float(traci.vehicle.getParameter(str("test_ebike"),"device.battery.maximumBatteryCapacity"))
+
+# set battery to 100% SOC-Level
+traci.vehicle.setParameter(str("test_ebike"),"device.battery.actualBatteryCapacity",maxBatteryCapacity)
+
+
 while traci.simulation.getMinExpectedNumber() > 0:
     traci.simulationStep()
+    actualBatteryCapacity = (float(traci.vehicle.getParameter(str("test_ebike"),"device.battery.actualBatteryCapacity")))
+    print("SOC: ",str(round(actualBatteryCapacity/maxBatteryCapacity *100,2)))
+    print("3D-Position: ",traci.vehicle.getPosition3D("test_ebike"))
 traci.close()  
